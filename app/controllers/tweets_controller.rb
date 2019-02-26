d0012f55d4b07c3cc5a9911fe5329c89f8061d05
valeria campilongo <cvaleriac@hotmail.it>
2019-02-26 23:18:26 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,82 +1,19 @@
 class TweetController < ApplicationController
 
-  get '/tweets' do
-    if Helpers.is_logged_in?(session)
-      @tweets = Tweet.all
-      erb :'/tweets/tweets'
-    else
-      redirect to '/users/login'
-    end
-  end
+end
 
-  get '/tweets/new' do
-    user = Helpers.current_user(session)
-    if user.nil?
-      redirect to '/login'
-    else
-      erb :'tweets/create_tweet'
-    end
+ erb :"/users/login"
+end
 
-  end
+post '/login' do
+ user = User.find_by(:username => params["username"])
 
-  post '/tweets' do
-    user = Helpers.current_user(session)
-    if user.nil?
-      redirect to '/login'
-    elsif params[:tweet][:content].empty?
-      redirect to '/tweets/new'
-    else
-      user.tweets.build({content: params[:tweet][:content]})
-      user.save
-    end
-    redirect to '/tweets'
-  end
-
-
-  get '/tweets/:id' do
-    redirect to '/login' unless Helpers.is_logged_in?(session)
-    @tweet = Tweet.find(params[:id])
-
-    erb :'/tweets/show_tweet'
-  end
-
-  get '/tweets/:id/edit' do
-    redirect to '/login' unless Helpers.is_logged_in?(session)
-
-    @tweet = Tweet.find(params[:id])
-    if @tweet.user == Helpers.current_user(session)
-        erb :'/tweets/edit_tweet'
-    else
-      redirect to '/login'
-    end
-  end
-
-  patch '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
-
-    if params[:tweet][:content].empty?
-      redirect to "/tweets/#{@tweet.id}/edit"
-    end
-
-    @tweet.update(params[:tweet])
-    @tweet.save
-    redirect to "/tweets/#{@tweet.id}"
-
-  end
-
-  delete '/tweets/:id/delete' do
-    if Helpers.is_logged_in?(session)
-      @tweet = Tweet.find(params[:id])
-      if @tweet.user == Helpers.current_user(session)
-        @tweet = Tweet.find_by_id(params[:id])
-        @tweet.delete
-        redirect to '/tweets'
-      else
-        redirect to '/tweets'
-      end
-    else
-      redirect to '/login'
-    end
-  end
-
+ if user && user.authenticate(params[:password])
+   session[:user_id] = user.id
+   redirect to '/tweets'
+ else
+   flash[:login_error] = "Incorrect login. Please try again."
+   redirect to '/login'
+ end
+end
 end
