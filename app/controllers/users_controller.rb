89596460ba0e94b39296d7c4f6ec72d2a932c297
valeria campilongo <cvaleriac@hotmail.it>
2019-02-26 23:24:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,51 +4,40 @@ class UserController < ApplicationController
     if Helpers.is_logged_in?(session)
       redirect to '/tweets'
     end
-    erb :'/users/create_user'
+
+    erb :"/users/create_user"
   end
 
   post '/signup' do
-    if !(params.has_value?(""))
-      user = User.create(params)
-      session["user_id"] = user.id
-      redirect to '/tweets'
-    else
-      redirect to '/signup'
+    params.each do |label, input|
+      if input.empty?
+        flash[:new_user_error] = "Please enter a value for #{label}"
+        redirect to '/signup'
+      end
     end
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/tweets'
   end
 
   get '/login' do
     if Helpers.is_logged_in?(session)
       redirect to '/tweets'
     end
-    erb :'/users/login'
+
+    erb :"/users/login"
   end
 
   post '/login' do
-  user = User.find_by(:username => params[:username])
-  	if user && user.authenticate(params[:password])
-      session["user_id"] = user.id
-      redirect to "/tweets"
-    else
-      redirect to "/login"
-    end
-	end
+    user = User.find_by(:username => params["username"])
 
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    if !@user.nil?
-      erb :'/users/user_tweets'
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
     else
+      flash[:login_error] = "Incorrect login. Please try again."
       redirect to '/login'
     end
   end
-
-  get '/logout' do
-    if Helpers.is_logged_in?(session)
-      session.clear
-    else
-      redirect to '/'
-    end
-    redirect to '/login'
-  end
-end
