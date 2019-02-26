ae5f197d7eee313efd53dda169d5a27895fc112c
valeria campilongo <cvaleriac@hotmail.it>
2019-02-26 23:19:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
+
 
 class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
-		set :session_secret, "password_security"
-  end
+  enable :sessions
+  use Rack::Flash
 
   get '/' do
     erb :index
